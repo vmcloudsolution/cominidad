619a625806eaef9b0dd7c7fc28377133353d1d65
Elmer Ventocilla <eventocilla@vmcloudsolution.pe>
2016-04-06 22:08:03 -0500

    carga inicial de modulos

---
+++ l10n_pe_ruc/README.md
@@ -0,0 +1,12 @@
+# l10n_pe_ruc
+Este addon valida el ruc de los clientes ODOO con la SUNAT  
+
+Librerias necesarias:  
+
+sudo apt-get install tesseract-ocr tesseract-ocr-eng python-imaging python-pip python-bs4 
+
+sudo pip install pytesseract
+
+Video de Muestra:
+
+https://youtu.be/RXv5CkkGEtc
---
+++ l10n_pe_ruc/__openerp__.py
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Alexander Cuellar Morales
+
+##############################################################################
+
+{
+    'name': 'Validador de Ruc',
+    'version': '1.0',
+    'category': 'Generic Modules/Base',
+    'description': """
+    Este addon valida el ruc de los clientes con la SUNAT
+
+    Video de muestra:
+    
+    https://youtu.be/RXv5CkkGEtc
+    
+    Librerias necesarias:
+
+    sudo apt-get install tesseract-ocr tesseract-ocr-eng python-imaging python-pip python-bs4 && sudo pip install pytesseract
+    
+    
+    """,
+    'author': 'Alexander Cuellar Morales alexcuellar@live.com',
+    'depends': ['base_vat'],
+    'website': '',
+    'installable': True,
+    'active': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
